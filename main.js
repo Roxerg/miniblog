be2731a95ed8f792bd94c076ecdfb0713669bd64
roxerg <roxguda@gmail.com>
2019-01-21 01:53:17 +0000

    added some design and minimum dimensions check

--- main.js
+++ main.js
@@ -22,8 +22,16 @@ $(document).mousemove(function(event) { updateSize(event); } );
 
 function updateSize(e) {
     if (current_card != null) {
+
+        var move_X = e.clientX-$(current_card.target).position().left
+        var move_Y = e.clientY-$(current_card.target).position().top;
+
+        if (move_X >= 200) {
         $(current_card.target).width(e.clientX-$(current_card.target).position().left);
+        }
+        if (move_Y >= 200) {
         $(current_card.target).height(e.clientY-$(current_card.target).position().top);
+        }
     }
 }
 
