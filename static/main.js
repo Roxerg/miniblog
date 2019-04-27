870d4e29e0c6f61a380754c093437b602a00c287
roxerg <roxguda@gmail.com>
2019-04-27 01:57:38 +0100

    finished up top menu, fixed hiding/revealing posts

--- static/main.js
+++ static/main.js
@@ -23,7 +23,17 @@ $(document).mousedown(function(event) {
 
 
 
+function toggleExpand(elem) {
+    var card = elem.parentNode
+    var cardtext = card.querySelector("#card-text");
+    cardtext.classList.toggle("card-text-toggle");
+    card.classList.toggle("card-toggle");
 
+    var btns = card.getElementsByClassName("twitter-share-button")
+    btns[0].classList.toggle("hidden");
+    btns[1].classList.toggle("hidden");
+    btns[2].classList.toggle("hidden");
+}
 
 
 var nodes = document.getElementsByClassName("cutoff");
@@ -56,6 +66,18 @@ document.querySelector('.button').addEventListener('click', () => {
 var hover_main = false;
 var hover_second = false;
 
+
+document.querySelector('.rokasg').addEventListener("click", () => {
+    
+    if (hover_main) {
+        hover_main = false;
+    }
+    else {
+        hover_main = true;
+    }
+    
+    hoverHandler();
+}, false)
 
 document.querySelector('.rokasg-menu').addEventListener("mouseover", () => {
     hover_second = true;
@@ -102,14 +124,14 @@ containingDiv.addEventListener('click', function(e) {
     console.log(e.target.tagName);
 
 
-  if("card-text" == e.target.getAttribute("id")) {
-      window.location.href = e.target.getAttribute("href");
-  }
+  //if("card-text" == e.target.getAttribute("id")) {
+  //    window.location.href = e.target.getAttribute("href");
+  //}
 
-  if("P" == e.target.tagName || "H2" == e.target.tagName || "H3" == e.target.tagName || "H4" == e.target.tagName || "H1" == e.target.tagName) {
-    console.log("fuck me")  
-    window.location.href = e.target.parentNode.getAttribute("href");
-  }
+  //if("P" == e.target.tagName || "H2" == e.target.tagName || "H3" == e.target.tagName || "H4" == e.target.tagName || "H1" == e.target.tagName) {
+    
+  //  window.location.href = e.target.parentNode.getAttribute("href");
+  //}
     
   if('button-1' == e.target.getAttribute("id")) {
     e.target.parentNode.remove();
